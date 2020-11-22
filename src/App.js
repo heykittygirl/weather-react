e49111a0170956998ca9ec4e64beab78458fb9ad
heykittygirl <68250883+heykittygirl@users.noreply.github.com>
2020-11-22 09:48:34 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import Search from "./Search";
-import Currentweather from "./Currentweather";
-import Footer from "./Footer";
 
 export default function App() {
   return (
@@ -10,7 +8,43 @@ export default function App() {
       <div className="container">
         <div className="weather-app">
           <Search />
-          <Currentweather />
+          <h1 id="city">Portland</h1>
+          <ul className="date-and-condition">
+            <li>
+              <span className="current-date">Sunday 9:42</span>
+            </li>
+            <li>
+              <span id="condition">Cloudy</span>
+            </li>
+          </ul>
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://openweathermap.org/img/wn/04d@2x.png"
+                alt="clouds"
+                id="weather-icon"
+              />
+
+              <span className="temp" id="temp">
+                37
+              </span>
+              <span id="units">
+                <button id="fahrenheit-link">°F</button> |
+                <button id="celcius-link">°C</button>
+              </span>
+            </div>
+
+            <div className="col-md-6">
+              <ul className="weather-details">
+                <li>
+                  Humidity: <span id="humidity">94</span>%
+                </li>
+                <li>
+                  Wind: <span id="wind">4</span> mph
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <footer className="source-code" id="source-code">
           <a
